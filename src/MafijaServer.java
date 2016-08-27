d2126d346648a2bd2a270b6fa23336088a7717e8
Gordan <Gordan@Thekgiga>
2016-08-28 01:05:26 +0200

    Odradjena konekcija na bazu. Registrovanje i prijavljivanje igraca.Sredjeni Exceptioni(server nije pokrenut,server pokrenut vise puta, kada igrac unese neki od nedozvoljenih znakova... ). prepravljen gui.

--- src/MafijaServer.java
+++ src/MafijaServer.java
@@ -2,57 +2,166 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
 
+import java.sql.*;
 
 public class MafijaServer implements Runnable {
-	
+
 	static LinkedList<Igrac> igraci = new LinkedList<Igrac>();
 	static LinkedList<ChatNitZaSve> igraci2 = new LinkedList<ChatNitZaSve>();
 	static int partijaId = 0;
-	
+	static boolean uspesnoUlogovan = false;
+
 	public static void main(String[] args) {
 		try {
 			ServerSocket serverskiSoket = new ServerSocket(2906);
 			new Thread(new MafijaServer()).start();
+
+			// dva polja za unos sa pocetnog prozora.
+			String user = null;
+			String password = null;
+
 			while (true) {
 				Socket klijentskiSoket = serverskiSoket.accept();
 				BufferedReader ulaz = new BufferedReader(new InputStreamReader(klijentskiSoket.getInputStream()));
+				PrintStream izlaz = new PrintStream(klijentskiSoket.getOutputStream());
 				
-				igraci.add(new Igrac(ulaz.readLine(), klijentskiSoket));
-				
+				//petlja se vrti dok se uspesno ne izvrsi registrovanje ili login novog igraca
+				while (!uspesnoUlogovan) {
+					String[] data = ulaz.readLine().split(";");
+					if (data.length >= 1) {
+						user = data[0];
+					}
+					if (data.length >= 2) {
+						password = data[1];
+					}
+					// proeverava da li klijent zeli da se registruje.
+					boolean registruj = false;
+					if (data.length == 3) {
+						if (data[2].equals("REGISTRUJ")) {
+							registruj = true;
+						}
+					}
+
+					// konekncija na bazu i te zafrkancije
+					Connection con;
+					Statement st;
+					ResultSet rs;
+
+					try {
+
+						String db = "jdbc:ucanaccess://baza.accdb";
+						con = DriverManager.getConnection(db);
+						st = con.createStatement();
+						String sql;
+
+						// proces registracije se obavlja ako je registruj == true u suprotnom se radi proces login-ovonja
+						if (registruj) {
+							sql = "insert into Table1 values('" + user + "','" + password + "')";
+							// provera da li postoji vec registrovan korisnik sa
+							// tim imenom
+							String sqlDaProveriDaLiPostojiNekoSaTimImenom = "select USERNAME from Table1 where USERNAME = '"
+									+ user + "'";
+							rs = st.executeQuery(sqlDaProveriDaLiPostojiNekoSaTimImenom);
+
+							int count1 = 0;
+							while (rs.next()) {
+								count1++;
+							}
+							
+							//Ne postoji neko u bazi, sa tim imenom
+							if (count1 == 0) {
+								
+
+								// executeUpdate() se koristi kod insert u
+								// tabelu, vraca broj koji govori koliko novih
+								// redova je ubaceno u tabelu
+								int pom = st.executeUpdate(sql);
+
+								if (pom == 1) {
+									// System.out.println(user + ", USPESNO STE SE REGISTROVALI.");
+									izlaz.println("OK");
+									uspesnoUlogovan = true;
+								} else {
+									//System.out.println("Nije uspela registracija");
+									izlaz.println("NOTOK");
+
+								}
+
+							} else {
+								//System.out.println("Korisnik " + user + "  vec postoji u bazi.");
+								izlaz.println("NOTOK");
+
+							}
+
+						} else {// ovo je obican proces logovanja, bez registracije
+							sql = "select USERNAME,PASSWORD from Table1 where USERNAME = '" + user
+									+ "' AND PASSWORD = '" + password + "'";
+							// executeQuery() se koristi za citanje podataka.
+							rs = st.executeQuery(sql);
+
+							int count = 0;
+							while (rs.next()) {
+								count = count + 1;
+							}
+
+							if (count == 1) {
+								System.out.println("Uspesno ste se ulogovali.");
+								izlaz.println("OK");
+								uspesnoUlogovan = true;
+							} else {
+								System.out.println("Korisnik " + user + " sa tim passwordom ne postoji u bazi.");
+								izlaz.println("NOTOK");
+
+							}
+						}
+
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+				uspesnoUlogovan = false;
+
+				igraci.add(new Igrac(user, klijentskiSoket));
+
 				if (igraci.size() == 6) {
 					Partija p = new Partija(igraci, partijaId);
 					p.run();
+
 				}
-				
+
 			}
-			
+
+		} catch (BindException e) {
+			System.out.println("SERVER JE VEC POKRENUT");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public void run(){
+
+	public void run() {
 		try {
-			ServerSocket serSoket=new ServerSocket(2907);
-			
-			
-			while(true){
+			ServerSocket serSoket = new ServerSocket(2907);
+
+			while (true) {
 				Socket klijentskiS = serSoket.accept();
 				BufferedReader ulaz2 = new BufferedReader(new InputStreamReader(klijentskiS.getInputStream()));
 				igraci2.add(new ChatNitZaSve(klijentskiS, igraci2, ulaz2.readLine()));
 				igraci2.getLast().start();
-				
-				
+
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
